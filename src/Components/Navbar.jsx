b05c3e50e431d11e86021bf9f039bbc4ee8216ae
JunuThapa <junuthapa011@gmail.com>
2023-07-11 23:01:50 -0400

    changed app.css

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = ({ children, value, className }) => {
+  return (
+    <div>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
