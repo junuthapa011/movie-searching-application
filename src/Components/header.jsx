d463f80775730b0fb94cc65427f317c488d807c3
JunuThapa <junuthapa011@gmail.com>
2023-07-13 00:56:36 -0400

    add header logo

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { MdMovieFilter } from "react-icons/md";
 
 function Header() {
   return (
-    <div className="flex justify-center items-center h-[40px] bg-gradient-to-r from-indigo-200 to-blue-400 ml-[275px]">
-      <p>THE MOVIE DB</p>
+    <div className="flex justify-center items-center h-[40px] bg-gradient-to-r from-indigo-200 to-blue-400 ml-[275px] font-bold">
+      <p className="pr-2">THE MOVIE DB</p>
+      <MdMovieFilter />
     </div>
   );
 }
