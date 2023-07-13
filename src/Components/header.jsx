b65a9afeefb6fd11f19173a7a12d1fe1b38e12fe
JunuThapa <junuthapa011@gmail.com>
2023-07-13 00:58:19 -0400

    change header's position

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -3,7 +3,7 @@ import { MdMovieFilter } from "react-icons/md";
 
 function Header() {
   return (
-    <div className="flex justify-center items-center h-[40px] bg-gradient-to-r from-indigo-200 to-blue-400 ml-[275px] font-bold">
+    <div className="fixed w-full flex justify-center items-center h-[40px] bg-gradient-to-r from-indigo-200 to-blue-400 font-bold">
       <p className="pr-2">THE MOVIE DB</p>
       <MdMovieFilter />
     </div>
